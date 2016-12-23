a220a0ba0bc8fac5fac9381faf3ca8c050ce5b9a
Tian Chengyuan <395750080@qq.com>
2016-12-23 10:50:01 +0800

    入驻页面完成

--- page/project/scripts/controllers/user/CompanyCreateController.js
+++ page/project/scripts/controllers/user/CompanyCreateController.js
@@ -1,10 +1,32 @@
-function CompanyCreateController($scope,FileService) {
+function CompanyCreateController($scope,FileService,CompanyService,GlobalService) {
   console.log("载入CompanyCreateController");
-  
-  $scope.upload = function(file){
-  	FileService.uploadFile(file).then(function(result) {
-  		alert("上传成功");
-  	});
+  //初始化
+  $scope.ctypeList = GlobalService.companyType;
+  $scope.cmptype = $scope.ctypeList[0];
+  $(".form_datetime").datetimepicker({format:'YYYY/MM/DD'});
+  //交互
+  $scope.uploadLogo = function(file){
+  	if(file){
+  		$scope.cmpLogo = file;
+		FileService.uploadFile(file).then(function(result) {
+  			$scope.company.logo = result.urls[0];
+  		});
+	}
   }
-  
+  $scope.uploadCertificate = function(file){
+  	if(file){
+  		FileService.uploadFile(file).then(function(result) {
+  			$scope.company.info = result.urls[0];
+  		});
+  	}
+  }
+  $scope.registerCompany  = function(){
+  		//console.log($scope.cmptype);
+  		$scope.company.type = $scope.cmptype.id;
+  		console.log($scope.company);
+  		$scope.company.address="";
+  		CompanyService.registerCompany($scope.company).then(function(result){
+  			alert("注册成功");
+  		});
+  }
 }
--- page/project/scripts/services/FileService.js
+++ page/project/scripts/services/FileService.js
@@ -3,20 +3,21 @@ angular.module("auto-biz-user")
   	//上传文件
   	this.uploadFile = function(file){
   		var deferred = $q.defer();
+  		if(file){
 		Upload.upload({
 			url: GlobalService.baseUrl+'upload',
 			file: file
 		}).success(function (res) {
 			if(res.callStatus=="SUCCEED"){
+				console.log("上传文件成功");
 				deferred.resolve(res.data);
 			}else{
 				alert("您好，上传文件失败");
-          		deferred.resolve();
 			}
 		}).error(function(){
 			alert("您好，上传文件失败");
-          	deferred.resolve();
 		});
+		}
 		return deferred.promise;
 	};
 });
