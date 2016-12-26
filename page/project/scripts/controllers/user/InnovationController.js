b6306cb656494734a164f0646917168e5f9462c5
Tian Chengyuan <395750080@qq.com>
2016-12-26 14:56:00 +0800

    企业列表

--- page/project/scripts/controllers/user/InnovationController.js
+++ page/project/scripts/controllers/user/InnovationController.js
@@ -5,8 +5,10 @@ function InnovationController($scope,GlobalService,CompanyNewsService) {
 	$scope.currentPage=$scope.innovationList[0][0];
 
 	$scope.cmpNews = {
-		currentPage:1,
-		pagePerNum:10
+		currentPage:-1,
+		pagePerNum:10,
+		totalNum:-1,
+		totalPage:-1,
 	}
 	// CompanyNewsService.getCompanyNewsList("","","","","",$scope.cmpNews.currentPage,$scope.cmpNews.pagePerNum).then(function(result){
 	// 	$scope.cmpNewsList = result;
@@ -14,4 +16,10 @@ function InnovationController($scope,GlobalService,CompanyNewsService) {
 	$scope.cmpNewsList = [{"title":"bl2321","author":"auor","isFirst":"1","isOnline":"1","company":"XJ2","tag":"1","desc":"122331","pic":"111","wysiwyg":"cntent"},
 	{"title":"bl2321","author":"auor","isFirst":"1","isOnline":"1","company":"XJ2","tag":"1","desc":"122331","pic":"111","wysiwyg":"cntent"}];
 	//$scope.newsList = 
+	$scope.selectItem = function(item){
+		$scope.currentPage=item;
+	}
+	$scope.loadMore=function(){
+
+	}
 }
