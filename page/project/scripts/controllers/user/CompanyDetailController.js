2287724032c7dbae668d04a3810ce899f450ba43
humiheng <419292815@qq.com>
2016-12-28 19:37:06 +0800

    企业详情连通数据

--- page/project/scripts/controllers/user/CompanyDetailController.js
+++ page/project/scripts/controllers/user/CompanyDetailController.js
@@ -1,23 +1,28 @@
-function CompanyDetailController($scope,GlobalService,CompanyNewsService,$routeParams) {
+function CompanyDetailController($scope,GlobalService,CompanyNewsService,$routeParams,CompanyService) {
   console.log("CompanyDetailController");
-    $scope.leafCmpId = "585b7d66b6a493e45ea96060";  
-    $scope.companyDetail={
-    	"logo":"page/project/images/web_header_logo.png",
-    	"shortName":"恒昌好车",
-    	"areaList":[
-    		{"areaName":"汽车配件"},
-    		{"areaName":"汽车装饰"}
-    	],
-    	"shortCut":"互联网汽车金融平台O2O平台, 提供线上交易、金融解决方案、后市场服务",
-    	"city":"上海",
-    	"regTime":"2014/12/13",
-    	"isNeedInvest":"1"
-    }
-    if ( $scope.companyDetail.isNeedInvest == 1){
-    	 $scope.isNeedInvestContent = "有投融资需求";
-    }else{
-    	$scope.isNeedInvestContent = "暂无投融资需求";
-    }
+    $scope.leafCmpId = "585b7d66b6a493e45ea96060"; 
+    $scope.cmpId = $scope.leafCmpId;
+    CompanyService.getComppanyById($scope.cmpId).then(function(result){
+        $scope.companyDetail = result;
+        if ( $scope.companyDetail.isNeedCapital == 'true'){
+             $scope.isNeedInvestContent = "有投融资需求";
+        }else{
+            $scope.isNeedInvestContent = "暂无投融资需求";
+        }
+    }); 
+    // $scope.companyDetail={
+    // 	"logo":"page/project/images/web_header_logo.png",
+    // 	"shortName":"恒昌好车",
+    // 	"areaList":[
+    // 		{"areaName":"汽车配件"},
+    // 		{"areaName":"汽车装饰"}
+    // 	],
+    // 	"shortCut":"互联网汽车金融平台O2O平台, 提供线上交易、金融解决方案、后市场服务",
+    // 	"city":"上海",
+    // 	"regTime":"2014/12/13",
+    // 	"isNeedInvest":"1"
+    // }
+    
 
     $scope.cmpDetailList = GlobalService.cmpDetailList;
 	$scope.currentPage=$scope.cmpDetailList[0][0];
--- page/project/scripts/services/CompanyService.js
+++ page/project/scripts/services/CompanyService.js
@@ -1,11 +1,11 @@
 angular.module("auto-biz-user")
-  .service('CompanyService', function ($http,GlobalService,$q,AuthService) {
+.service('CompanyService', function ($http,GlobalService,$q,AuthService) {
   var self = this; 
   //注册公司
   this.registerCompany = function (company) {
     var deferred = $q.defer();
     $http.post(GlobalService.baseUrl+'company/signup',
-          company
+      company
       ).success(function (res) {
         console.log("企业注册");
         if(res.callStatus == "SUCCEED"){
@@ -17,78 +17,94 @@ angular.module("auto-biz-user")
         alert("您好，您访问的内容出错");
       });
       return deferred.promise;
-  };
-  
+    };
+    
   //获取企业列表
-    this.getCompanyList = function(numPerPage,pageNum,isPassed,type,name) {
-        var deferred = $q.defer();
-        console.log("读取getCompanyListService网络数据");
-        var url='http://123.56.220.72:3300/company/list/'+numPerPage+'/'+pageNum;
-        var paras = [];
-        if(isPassed!==''){
-            var obj1=new Object(); 
-            obj1.name="isPassed";
-            obj1.value=isPassed;
-            paras.push(obj1);
-        }
-         if(type!==''){
-            var obj2=new Object(); 
-            obj2.name="type";
-            obj2.value=type;
-            paras.push(obj2);
-        }
-        if(name!==''){
-            var obj3=new Object(); 
-            obj3.name="name";
-            obj3.value=name;
-            paras.push(obj3);
-        }
-        if(paras.length!==0){
-            url=url+'?'+paras[0].name+'='+paras[0].value;
-            for(var i=1;i<paras.length;i++){
-                console.log(i);
-                console.log('then:'+paras[i].name);
-                url=url+'&'+paras[i].name+'='+paras[i].value;
-            }
-        }
-        console.log(url);
-
-        $http.get(url)
-            .success(function(data, status, headers, config){
-                console.log(data);
-                if(data.callStatus=='SUCCEED'){
-                    deferred.resolve(data.data);
-                }
-                else{
-                    alert(data.errCode)
-                    deferred.resolve("");
-                }
-            })
-            .error(function(data, status, headers, config){
-                console.log(data);
-                deferred.reject(data);
-            });
-        return deferred.promise;
+  this.getCompanyList = function(numPerPage,pageNum,isPassed,type,name) {
+    var deferred = $q.defer();
+    console.log("读取getCompanyListService网络数据");
+    var url='http://123.56.220.72:3300/company/list/'+numPerPage+'/'+pageNum;
+    var paras = [];
+    if(isPassed!==''){
+      var obj1=new Object(); 
+      obj1.name="isPassed";
+      obj1.value=isPassed;
+      paras.push(obj1);
     }
+    if(type!==''){
+      var obj2=new Object(); 
+      obj2.name="type";
+      obj2.value=type;
+      paras.push(obj2);
+    }
+    if(name!==''){
+      var obj3=new Object(); 
+      obj3.name="name";
+      obj3.value=name;
+      paras.push(obj3);
+    }
+    if(paras.length!==0){
+      url=url+'?'+paras[0].name+'='+paras[0].value;
+      for(var i=1;i<paras.length;i++){
+        console.log(i);
+        console.log('then:'+paras[i].name);
+        url=url+'&'+paras[i].name+'='+paras[i].value;
+      }
+    }
+    console.log(url);
+
+    $http.get(url)
+    .success(function(data, status, headers, config){
+      console.log(data);
+      if(data.callStatus=='SUCCEED'){
+        deferred.resolve(data.data);
+      }
+      else{
+        alert(data.errCode)
+        deferred.resolve("");
+      }
+    })
+    .error(function(data, status, headers, config){
+      console.log(data);
+      deferred.reject(data);
+    });
+    return deferred.promise;
+  }
     //用户认证通过/否决
     this.passUser = function(id,passFlag) {
-        var token=AuthService.user.token;
-        var url='http://123.56.220.72:3300/user/modify/approval?token='+token+'&companyId='+id+'&approvalStatus='+passFlag;
-        var deferred = $q.defer();
-        $http.get(url)
-            .success(function(data, status, headers, config){
-                if(data.callStatus=='SUCCEED'){
-                    alert('认证成功!');
-                    deferred.resolve('');
-                }
-                else{
-                    alert('认证失败'+data.errCode)
-                    deferred.resolve('');
-                }
-            })
-            .error(function(data, status, headers, config){
-                deferred.reject(data);
-            });
-        return deferred.promise;
+      var token=AuthService.user.token;
+      var url='http://123.56.220.72:3300/user/modify/approval?token='+token+'&companyId='+id+'&approvalStatus='+passFlag;
+      var deferred = $q.defer();
+      $http.get(url)
+      .success(function(data, status, headers, config){
+        if(data.callStatus=='SUCCEED'){
+          alert('认证成功!');
+          deferred.resolve('');
+        }
+        else{
+          alert('认证失败'+data.errCode)
+          deferred.resolve('');
+        }
+      })
+      .error(function(data, status, headers, config){
+        deferred.reject(data);
+      });
+      return deferred.promise;
     }
-})
+
+    //根据cmpID获取企业基本信息
+    this.getComppanyById = function(cmpId){
+      var deferred = $q.defer();
+      $http.get(GlobalService.baseUrl+'company/detail?companyId='+cmpId).success(function (res) {
+        console.log("获取企业信息");
+        if(res.callStatus == "SUCCEED"){
+          deferred.resolve(res.data);
+        }else{
+          alert("您好，您访问的内容出错");
+        }
+      }).error(function (res){
+        alert("您好，您访问的内容出错");
+      });
+      return deferred.promise;
+    };
+  })
