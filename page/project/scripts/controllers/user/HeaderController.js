323de47de26e5fcb608e99d16f14509041a4f2d6
Tian Chengyuan <395750080@qq.com>
2017-02-06 11:47:33 +0800

    bug

--- page/project/scripts/controllers/user/HeaderController.js
+++ page/project/scripts/controllers/user/HeaderController.js
@@ -2,7 +2,7 @@ angular.module("auto-biz-user").controller("HeaderController",["$scope","GlobalS
   function HeaderController($scope,GlobalService,$filter,AuthService) {
     $scope.headerItems = [{name:"首页",title:"HOME",url:"http://www.auto-biz.cn"},{name:"资讯",title:"NEWS",url:"http://www.auto-biz.cn/toNews.html"},{name:"对话",title:"DIALOGUE",url:"http://www.auto-biz.cn/toTalks.html"},{name:"数据",title:"DATA",url:"http://www.auto-biz.cn/toData.html"},{name:"专栏",title:"COLUMNS",url:"http://www.auto-biz.cn/authors.html"},{name:"品车",title:"CARS",url:"http://www.auto-biz.cn/toTaste.html"},{name:"投稿",title:"CT",url:"http://www.auto-biz.cn/contribute.html"},{name:"企业平台",title:"PLATFORM",url:"/innovation"}];
     ;
-    $scope.currentPage = "innovation";
+    $scope.currentPage = "platform";
     getLoginState();
     function getLoginState(){
       if(AuthService.user){
