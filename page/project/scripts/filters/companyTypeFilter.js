c2e767908f01c6e31950e90641a1733c53e75ffb
humiheng <419292815@qq.com>
2017-03-03 12:35:14 +0800

    css change

--- page/project/scripts/filters/companyTypeFilter.js
+++ page/project/scripts/filters/companyTypeFilter.js
@@ -3,10 +3,10 @@ angular.module('auto-biz-user')
   return function(input) {
     //CM初创企业，CG互联网汽车，CS人工智能，NEC自动驾驶，NOC智慧交通，CC汽车金融，CE清洁新能源，PT汽车轻量化，MOC其它
     if(input=="CM") return "初创企业";
-    else if(input=="CG") return "互联网汽车";
+    else if(input=="CG") return "智能网联汽车";
     else if(input=="CS") return "人工智能";
-    else if(input=="NEC") return "自动驾驶";
-    else if(input=="NOC") return "智慧交通";
+    else if(input=="NEC") return "智慧交通";
+    else if(input=="NOC") return "汽车共享";
     else if(input=="CC") return "汽车金融";
     else if(input=="CE") return "清洁新能源";
     else if(input=="PT") return "汽车轻量化";
